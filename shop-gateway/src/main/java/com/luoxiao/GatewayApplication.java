cdff36eab5c2b65e9a3742bca880b7842d89ce30
mandelo <loshower@outlook.com>
2021-03-09 15:42:47 +0800

    added Gateway

--- shop-gateway/src/main/java/com/luoxiao/GatewayApplication.java
+++ shop-gateway/src/main/java/com/luoxiao/GatewayApplication.java
@@ -2,7 +2,6 @@ package com.luoxiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @description:
@@ -11,11 +10,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
